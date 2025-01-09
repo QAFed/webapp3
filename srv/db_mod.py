141da46d6e000a8158e5ce31bd109a841ac7fd82
f.idolenkov <idolenkov@gmail.com>
2025-01-09 14:19:17 +0300

    plus name change by ip

--- srv/db_mod.py
+++ srv/db_mod.py
@@ -13,3 +13,23 @@ class VMdata(db.Model):
     def __init__(self, name, ip):
         self.name = name
         self.ip = ip
+
+class Modif():
+    @staticmethod
+    def add_name_from_ip(data_json):
+        new_name = data_json.get('name')
+        find_ip = data_json.get('ip')
+        vm_string = VMdata.query.filter_by(ip=find_ip).first()
+        if vm_string:
+            vm_string.name = new_name
+        else:
+            new_vm_string = VMdata(name=new_name, ip=find_ip)
+            db.session.add(new_vm_string)
+        db.session.commit()
+
+
+
+
+
+
+
--- srv/srv.py
+++ srv/srv.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from db_mod import db, VMdata
+from flask import Flask, render_template, request, jsonify
+from db_mod import db, VMdata, Modif
 
 
 app = Flask(__name__)
@@ -12,6 +12,16 @@ db.init_app(app)
 def index():
     vm_data = VMdata.query.all()
     return render_template('index.html', vm_data=vm_data)
+
+@app.route('/update', methods=['POST'])
+def update_ip_table():
+    if not request.is_json:
+        return jsonify({'error':'Request must be json'}), 400
+
+    Modif.add_name_from_ip(request.get_json())
+
+    return jsonify({'message': 'Success'}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
