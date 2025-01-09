340b78bc12b696aae2179a5be5ffb1b829fd3e58
f.idolenkov <idolenkov@gmail.com>
2025-01-09 20:07:26 +0300

    forbot

---
+++ forbot.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+UNIQUE_ID="TST100"
+SERVER_URL="http://192.168.55.105:5000/update"
+IP_FILE="/tmp/current_ip.txt"
+LISTEN_PORT=8888
+
+get_current_ip() {
+  hostname -I | awk '{print $1}'
+}
+
+send_ip_to_server() {
+  local current_ip="$1"
+  echo "Sending IP: $current_ip to server..."  # Логирование отправляемого IP
+  response=$(curl -s -o /dev/null -w "%{http_code}" --location --request POST "$SERVER_URL" \
+    --header "Content-Type: application/json" \
+    --data "{\"ip\": \"$current_ip\",\"name\": \"$UNIQUE_ID\"}")
+  echo "Response code: $response"  # Логирование кода ответа
+  if [ "$response" -eq 200 ] || [ "$response" -eq 201 ]; then
+    echo "Server accepted IP (response code: $response). Writing to file."
+    echo "$current_ip" > "$IP_FILE"
+  else
+    echo "Server rejected IP. Response code: $response"
+  fi
+}
+
+start_listener() {
+  while true; do
+    echo -e "HTTP/1.1 200 OK\nContent-Type: application/json\n\n{\"ip\": \"$(get_current_ip)\", \"name\": \"  done
+}
+
+# Запуск сервера для ответов на запросы
+start_listener &
+
+while true; do
+  current_ip=$(get_current_ip)
+
+  if [ ! -f "$IP_FILE" ] || [ "$(cat $IP_FILE)" != "$current_ip" ]; then
+    send_ip_to_server "$current_ip"
+  fi
+  sleep 30
+done
